029d3b1fa7bb6fbd2997bf291cbe58a994ab9a0d
kaiiak <aNxFi37X@outlook.com>
2019-03-17 23:49:31 +0800

    Add logger

---
+++ logger.go
@@ -0,0 +1,9 @@
+package goesodm
+
+// Logger with error warn info debug trace
+type Logger interface {
+	Errorf(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+	Debugf(format string, args ...interface{})
+}
